dec693e137b62d24cad2971d74c30a4b015f34fc
Kaoru Nishikawa <k.nishikawa@a.phys.nagoya-u.ac.jp>
2022-08-02 14:51:19 +0900

    #2 Make all functionalities work nominally

--- vnc-entrypoint.sh
+++ vnc-entrypoint.sh
@@ -3,10 +3,10 @@
 while getopts 'n:d:g:p:' option
 do
     case "$option" in
-    	n) DISPLAYNO=$OPTARG ;;
-    	d) DEPTH=$OPTARG ;;
-    	g) GEOMETRY=$OPTARG ;;
-    	p) PASSWD=$OPTARG ;;
+        n) DISPLAYNO=$OPTARG ;;
+        d) DEPTH=$OPTARG ;;
+        g) GEOMETRY=$OPTARG ;;
+        p) PASSWD=$OPTARG ;;
     esac
 done
 
@@ -17,14 +17,14 @@ fi
 
 PASSWDPATH=/root/.vnc/passwd
 echo ${PASSWD:-password} | vncpasswd -f > $PASSWDPATH
+chmod 600 $PASSWDPATH
+
+gsettings set org.gnome.desktop.default-applications.terminal exec 'xterm'
 
 vncserver $DISPLAYNO \
     -depth ${DEPTH:-16} \
     -geometry ${GEOMETRY:-1024x768}
-#    -localhost no \
-#    -passwd $PASSWDPATH \
-#	-xstartup /root/.vnc/xstartup
 
 trap "vncserver -kill $DISPLAYNO; exit 0" SIGINT SIGKILL SIGTERM
-tail -f /root/.vnc/*$DISPLAYNO.log
-
+#tail -f /root/.vnc/*$DISPLAYNO.log
+while true; do sleep 5; done
